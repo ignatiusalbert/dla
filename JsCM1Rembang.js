414dbbf440298fa05bb99681b1da8363c809a77a
ignatiusalbert <65345298+ignatiusalbert@users.noreply.github.com>
2020-06-26 14:34:13 +0700

    update js cm1 rembang

--- JsCM1Rembang.js
+++ JsCM1Rembang.js
@@ -1,3 +1,3 @@
 var qrLastSupper = document.createElement("img");
-qrLastSupper.src = "CM1_Rbg_Looker.png";
+qrLastSupper.src ="https://github.com/ignatiusalbert/dla_msi_sig_project/blob/master/CM1_Rbg_Looker.png?raw=true";
 document.body.appendChild(qrLastSupper);
